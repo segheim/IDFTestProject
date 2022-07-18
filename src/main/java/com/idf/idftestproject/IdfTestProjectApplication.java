746b47898c9e2ab7a3b9e72d83fb21875374bf51
Savitsky Evgeni <zheni11seg@gmail.com>
2022-07-18 10:50:12 +0300

    init  project

---
+++ src/main/java/com/idf/idftestproject/IdfTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.idf.idftestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdfTestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdfTestProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idf/idftestproject/IdfTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idf.idftestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdfTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
